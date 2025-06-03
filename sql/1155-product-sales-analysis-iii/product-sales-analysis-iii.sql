8e9450b410e6ec05a9efa1697916b264f816fe48
dudewithcode <dudewithcode@outlook.com>
2025-06-03 23:01:36 +0300

    Time: 470 ms (99.19%) | Memory: 0B (100.00%) - LeetSync

--- sql/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ sql/1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,9 +1,5 @@
 /* Write your PL/SQL query statement below */
-select distinct p.product_id, s.year first_year,s.quantity, s.price
-from sales p
-
-join ( 
-    select * from sales
+    select product_id, year first_year, quantity, price
+    from sales
     where (product_id, year) in (
-            select product_id, min(year) year from sales group by product_id )) s
-on s.product_id = p.product_id
+            select product_id, min(year) year from sales group by product_id )
