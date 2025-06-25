62f6425fa4772721f753e9421827302383abb26b
dudewithcode <dudewithcode@outlook.com>
2025-06-25 21:19:55 +0300

    Time: 687 ms (58.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ sql/1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,12 @@
+/* Write your PL/SQL query statement below */
+with cum_queue as (select person_name, 
+ sum(weight) over(order by turn asc) cum_weight
+from queue),
+fil_queue as (
+select person_name, cum_weight
+from cum_queue
+where cum_weight <= 1000
+)
+
+select distinct first_value(person_name) over(order by cum_weight desc) person_name
+from fil_queue
