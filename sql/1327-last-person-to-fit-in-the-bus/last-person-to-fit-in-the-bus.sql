1f726e00ead11c50bf2975e3fc2cd4f02f6eb02c
dudewithcode <dudewithcode@outlook.com>
2025-06-25 21:29:49 +0300

    Time: 732 ms (43.16%) | Memory: 0B (100.00%) - LeetSync

--- sql/1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ sql/1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,12 @@
-SELECT person_name
-FROM (
-    SELECT person_name, 
-           SUM(weight) OVER (ORDER BY turn) AS cum_weight
-    FROM queue
-    ORDER BY cum_Weight DESC
-) 
-WHERE cum_weight <= 1000 and rownum = 1
+/* Write your PL/SQL query statement below */
+with cum_queue as (select person_name, 
+ sum(weight) over(order by turn asc) cum_weight
+from queue),
+fil_queue as (
+select person_name, cum_weight
+from cum_queue
+where cum_weight <= 1000
+)
+
+select distinct first_value(person_name) over(order by cum_weight desc) person_name
+from fil_queue
