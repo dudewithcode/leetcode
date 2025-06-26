4b9ef54e98cab3056614d0e0eeaa41267d95c2f0
dudewithcode <dudewithcode@outlook.com>
2025-06-26 22:05:15 +0300

    Time: 575 ms (97.81%) | Memory: 0B (100.00%) - LeetSync

--- sql/2057-count-salary-categories/count-salary-categories.sql
+++ sql/2057-count-salary-categories/count-salary-categories.sql
@@ -1,24 +1,23 @@
-/* Write your PL/SQL query statement below */
 with categories as (
-select 'Low Salary' category
-from dual
-union
-select 'Average Salary'
-from dual
-union
-select 'High Salary'
-from dual
-),
-cat_accounts as (
-select account_id,
-    case 
-    when income < 20000 then 'Low Salary'
-    when income > 50000 then 'High Salary'
-    else 'Average Salary'
-    end category
-from accounts)
-
-select b.category, nvl(count(a.account_id),0) accounts_count 
-from cat_accounts a
-right join categories b on a.category = b.category
-group by b.category
+        select 'Low Salary' category 
+        from dual
+        union 
+        select 'Average Salary' 
+        from dual
+        union 
+        select 'High Salary'
+        from dual
+)
+select b.category, nvl(a.accounts_count,0) accounts_count
+from (
+select 
+case when income < 20000 then 'Low Salary'
+    when income >= 20000 and income <= 50000 then 'Average Salary'
+    else 'High Salary'
+end category, count(account_id) accounts_count 
+from accounts
+group by case when income < 20000 then 'Low Salary'
+    when income >= 20000 and income <= 50000 then 'Average Salary'
+    else 'High Salary'
+end) a
+right join categories b on b.category = a.category
